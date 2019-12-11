387483d9d511f6aed1bb0a8a6db991acee1328c7
camilo <cristianctr97@gmail.com>
2019-12-10 20:49:33 -0500

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Pantalla1Component } from './component/pantalla1/pantalla1.component';
+import { Pantalla2Component } from './component/pantalla2/pantalla2.component';
+import { Pantalla3Component } from './component/pantalla3/pantalla3.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"pantalla1", component:Pantalla1Component, children:[{
+    path:"pantalla2", component:Pantalla2Component
+  }]},
+  {path:"pantalla2", component:Pantalla2Component},
+  {path:"pantalla3", component:Pantalla3Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pantalla1Component } from './component/pantalla1/pantalla1.component';
+import { Pantalla2Component } from './component/pantalla2/pantalla2.component';
+import { Pantalla3Component } from './component/pantalla3/pantalla3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pantalla1Component,
+    Pantalla2Component,
+    Pantalla3Component
   ],
   imports: [
     BrowserModule,
